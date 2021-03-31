34f3915b1f1d64087579cadef29c0ded63c9704f
Bryan Castro <bry2098@gmail.com>
2021-03-31 16:51:04 -0500

    add games

--- src/screens/TestEvaluativo.js
+++ src/screens/TestEvaluativo.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext, useEffect} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, BackHandler } from 'react-native';
 import HeaderGame from '../components/HeaderGame';
 import PreScreenGame from '../components/PreScreenGame';
 import { css_Crucigrama } from '../styles/GameStyle';
+import { AuthContext } from "../navigation/AuthProvider";
+import BaseCrucigrama from '../components/BaseCrucigrama';
 
 const TestEvaluativo = () => {
+    const { preGame, setPreGame} = useContext(AuthContext);
+
+    const backHandler = BackHandler.addEventListener("hardwareBackPress", ()=> setPreGame(true))
+
+    useEffect(() => {
+        
+        return () => backHandler.remove();
+    }, [])
     return ( 
         <View style={css_Crucigrama.Container}>
-            <HeaderGame image='book.png' name='CRUCIGRAMA'/>
-           <PreScreenGame  txtDialogo='Ahora veremos cuanto has aprendido' />
+            <HeaderGame name='Test Evaluativo'/>
+            {preGame ? <PreScreenGame txtDialogo='Ahora veremos cuanto has aprendido ¡Suerte!'/> : null}
+            {preGame ? null : <JuegoLectura />}
         </View>
-     );
+     ); 
 }
  
 export default TestEvaluativo;
+
+f
