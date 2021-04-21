fec9c1b0f1da385e04d87a170e085a803cb43cf5
Bryan Castro <bry2098@gmail.com>
2021-04-21 13:27:58 -0500

    Sistema de juego de oraciones implementado

--- src/screens/JuegoImagenes.js
+++ src/screens/JuegoImagenes.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Button,
   BackHandler,
+  TextInput ,
 } from "react-native";
 import HeaderGame from "../components/HeaderGame";
 import PreScreenGame from "../components/PreScreenGame";
@@ -14,6 +15,10 @@ import Ruleta from "../components/Ruleta";
 import { AuthContext } from "../navigation/AuthProvider";
 import { Colors } from "../styles/Colors";
 import { css_JuegoImagenes, css_Vocabulario } from "../styles/GameStyle";
+import AwesomeAlert from "react-native-awesome-alerts";
+import LoseGame from "../screens/LoseGame";
+import WinGame from "../screens/WinGame";
+
 
 const JuegoImagenes = () => {
   const { itemRuleta, setItemRuleta, preGame, setPreGame } = useContext(
@@ -22,7 +27,9 @@ const JuegoImagenes = () => {
   // State
   const [item, setItem] = useState("");
   const [oracion, setOracion] = useState("");
-  const [respuesta, setRespuesta] = useState("");
+  const [alert1, setAlert1] = useState(false);
+  const [win, setWin] = useState(false);
+  const [lose, setLose] = useState(false);
   //
   const backHandler = BackHandler.addEventListener("hardwareBackPress", () =>
     setItemRuleta(null),
@@ -31,6 +38,7 @@ const JuegoImagenes = () => {
   const RuletaView = () => <Ruleta />;
   const Imagenes = ({ Tipo }) => {
     if (Tipo == "DRAGON") {
+      setOracion('El dragón sale a volar por las montañas buscando su alimento');
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -69,6 +77,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "PLATO") {
+      setOracion('El sol sale en la mañana y los niños salen a jugar despues de almorzar');
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -93,7 +102,7 @@ const JuegoImagenes = () => {
             </View>
             <View style={css_JuegoImagenes.Ejemplos}>
               <Text style={css_JuegoImagenes.txtEjemplos}>
-                Despues de almorzar
+                despues de almorzar
               </Text>
             </View>
             <View style={css_JuegoImagenes.Ejemplos}>
@@ -107,6 +116,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "GATO") {
+      setOracion('El gato y el perro juegan en el parque')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -141,6 +151,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "CANDADO") {
+      setOracion('Maria luego de salir de clase abre el candado de la puerta para ingresar a su casa')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -179,6 +190,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "FOSFORO") {
+      setOracion('Para endender la cocina usamos un fósforo y así podremos cocinar')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -217,6 +229,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "LIBRO") {
+      setOracion('Lucas al salir de clase hace todas sus tareas y luego se va jugar al parque')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -255,6 +268,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "TAPILLA") {
+      setOracion('La tapa es muy bonita sus curvas son como las flores y las nubes del cielo')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -293,6 +307,7 @@ const JuegoImagenes = () => {
     }
 
     if (Tipo == "REGALO") {
+      setOracion('La niña está muy feliz porque le regalaron un bonito vestido')
       return (
         <View style={css_JuegoImagenes.divJuego}>
           <View style={css_JuegoImagenes.divImgOracion}>
@@ -332,12 +347,69 @@ const JuegoImagenes = () => {
 
   };
   const ActividadImagenes = ({ Nombre }) => {
+   
+    const [respuesta, setRespuesta] = useState("");
+
+      //Verificamos si la oración escrita es correcta
+  const VerificarOracion = (respuesta)=>{
+    // Verificamos si la respuesta no está vacia
+    if(respuesta.length <= 15) return  setAlert1(true); 
+    // Verificamos si las oraciones son iguales => respuesta == oracion ? true : false  
+    if(respuesta == oracion){
+      setItem("")
+      setWin(true);      
+    }else{
+      setItem("")
+      setLose(true);      
+    }
+  }
+  
     return (
       <View>
+      <AwesomeAlert
+        // Alerta si el usuario no escribe su nombre
+        show={alert1}
+        showProgress={false}
+        title="Ops!"
+        message="Primero escribe una oración!!"
+        closeOnTouchOutside={true}
+        closeOnHardwareBackPress={false}
+        showCancelButton={false}
+        showConfirmButton={true}
+        confirmText="Aceptar"
+        confirmButtonColor={Colors.yellow}
+        onConfirmPressed={() => setAlert1(false)}
+      />
         <Imagenes Tipo={Nombre} />
+        <View>
+        <TextInput
+          style={css_JuegoImagenes.InputOracion}
+          onChangeText={setRespuesta}
+          value={respuesta}
+          multiline={true}
+          numberOfLines={4}
+          placeholder='Escribe correctamente la oración'
+          placeholderTextColor={Colors.white}
+        />
+      <TouchableOpacity
+      style={css_Boton.btnOpcion}
+      onPress={() => VerificarOracion(respuesta)}
+    >
+      <View style={css_Boton.divIcon}>
+        <Image
+          source={require("../img/mando.png")}
+          style={css_Boton.iconOpcion}
+        />
+      </View>
+      <Text style={css_Boton.txtOpcion}>Terminar</Text>
+    </TouchableOpacity>
+        </View>
       </View>
     );
+
+
   };
+
 
   useEffect(() => {
     setItem(itemRuleta);
@@ -349,10 +421,12 @@ const JuegoImagenes = () => {
     <View style={css_JuegoImagenes.Container}>
       <HeaderGame image="book.png" name="JUEGO DE IMÁGENES" />
       {preGame ? <PreScreenGame txtDialogo='Gira la ruleta y arma la oración correctamente ¡SUERTE!'/> : null }
-      {item == null && preGame == false ? <RuletaView /> : null}
+      {item == null && preGame == false && win !== true && lose !== true ? <RuletaView /> : null}
+      {win ? <WinGame Siguiente='Menu' Win={true} NombreTrofeo='Master de las Oraciones' /> : null }
+      {lose ? <LoseGame Siguiente="Menu" /> : null}
       {item == "DRAGON" ? <ActividadImagenes Nombre="DRAGON" /> : null}
       {item == "LIBRO" ? <ActividadImagenes Nombre="LIBRO" /> : null}
-      {item == "REGALO" ? <ActividadImagenes Nombre="REGALO" /> : null}
+      {item == "REGALO" ? <ActividadImagenes Nombre="REGALO" /> : null} 
       {item == "GATO" ? <ActividadImagenes Nombre="GATO" /> : null}
       {item == "CANDADO" ? <ActividadImagenes Nombre="CANDADO" /> : null}
       {item == "FOSFORO" ? <ActividadImagenes Nombre="FOSFORO" /> : null}
@@ -367,11 +441,11 @@ const css_Boton = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: Colors.turquesa,
     padding: 4,
-    width: "66%",
+    width: 230,
     borderRadius: 30,
     alignSelf: "center",
     alignItems: "center",
-    marginTop: "24%",
+    marginTop: '10%',
     margin: "2%",
     shadowColor: "#000",
     shadowOffset: {
@@ -401,7 +475,7 @@ const css_Boton = StyleSheet.create({
     fontWeight: "bold",
     textShadowRadius: 3,
     textShadowColor: "black",
-    marginHorizontal: "2%",
+    marginHorizontal: "5%",
     textShadowOffset: { width: 1, height: 1 },
     textTransform: "uppercase",
   },
--- src/styles/GameStyle.js
+++ src/styles/GameStyle.js
@@ -2,274 +2,281 @@ import { StyleSheet } from "react-native";
 import { Colors } from "./Colors";
 
 export const css_SopaPalabras = StyleSheet.create({
-    Container:{
-        flex:1,
-        backgroundColor: Colors.blue_dark,
+  Container: {
+    flex: 1,
+    backgroundColor: Colors.blue_dark,
+  },
+  EligeJuego: {
+    backgroundColor: Colors.turquesa,
+    width: "70%",
+    padding: "2%",
+    shadowColor: "#fff",
+    shadowOffset: {
+      width: 0,
+      height: 8,
     },
-    EligeJuego:{
-        backgroundColor: Colors.turquesa,
-        width: '70%',
-        padding: '2%',
-        shadowColor: '#fff',
-        shadowOffset: {
-            width: 0,
-            height: 8,
-        },
-        shadowOpacity: 0.44,
-        shadowRadius: 10.32,
+    shadowOpacity: 0.44,
+    shadowRadius: 10.32,
 
-        elevation: 16,
+    elevation: 16,
 
-        borderRadius: 50,
-        margin: '4%',
-        marginTop: '8%',
-
-    },
-    txtElige:{
-        fontSize: 18, 
-        color: Colors.blue_dark,
-        fontWeight: 'bold',
-        textShadowRadius:5,
-        textShadowColor: '#fff',
-        textShadowOffset: { width: 1, height: 2},
-        textAlign: 'center'
-    },
-    imgOso:{
-        alignSelf: 'center',
-        width: 145,
-        height: 170,
-        marginLeft: '8%'
-    },
-    btnOpcion:{
-        flexDirection: 'row',
-        backgroundColor: Colors.turquesa,
-        padding: 4,
-        width: '85%',
-        borderRadius: 30,
-        alignSelf: 'center',
-        alignItems: 'center',
-        margin: '2%',
-    },
-    divIcon:{
-       backgroundColor: Colors.yellow,
-       width: '20%',
-       height: 50,
-       borderRadius: 30
-    },
-    iconOpcion: {
-        resizeMode: 'contain',
-        width: '75%',
-        height: 50,
-        alignSelf: 'center'
-    },
-    txtOpcion:{
-        fontSize: 24,
-        color: Colors.blue_dark,
-        fontWeight: 'bold',
-        textShadowRadius: 2,
-        textShadowColor: Colors.yellow,
-        textShadowOffset: { width: 2, height: 1},
-        marginHorizontal: '4%',
-        textTransform: 'uppercase'
-    },
-    txtOpcion2:{
-        fontSize: 18, 
-        color: Colors.blue_dark,
-        fontWeight: 'bold',
-        textShadowRadius: 2,
-        textShadowColor: Colors.yellow,
-        textShadowOffset: { width: 2, height: 1},
-        marginHorizontal: '8%',
-    }
-
-})
+    borderRadius: 50,
+    margin: "4%",
+    marginTop: "8%",
+  },
+  txtElige: {
+    fontSize: 18,
+    color: Colors.blue_dark,
+    fontWeight: "bold",
+    textShadowRadius: 5,
+    textShadowColor: "#fff",
+    textShadowOffset: { width: 1, height: 2 },
+    textAlign: "center",
+  },
+  imgOso: {
+    alignSelf: "center",
+    width: 145,
+    height: 170,
+    marginLeft: "8%",
+  },
+  btnOpcion: {
+    flexDirection: "row",
+    backgroundColor: Colors.turquesa,
+    padding: 4,
+    width: "85%",
+    borderRadius: 30,
+    alignSelf: "center",
+    alignItems: "center",
+    margin: "2%",
+  },
+  divIcon: {
+    backgroundColor: Colors.yellow,
+    width: "20%",
+    height: 50,
+    borderRadius: 30,
+  },
+  iconOpcion: {
+    resizeMode: "contain",
+    width: "75%",
+    height: 50,
+    alignSelf: "center",
+  },
+  txtOpcion: {
+    fontSize: 24,
+    color: Colors.blue_dark,
+    fontWeight: "bold",
+    textShadowRadius: 2,
+    textShadowColor: Colors.yellow,
+    textShadowOffset: { width: 2, height: 1 },
+    marginHorizontal: "4%",
+    textTransform: "uppercase",
+  },
+  txtOpcion2: {
+    fontSize: 18,
+    color: Colors.blue_dark,
+    fontWeight: "bold",
+    textShadowRadius: 2,
+    textShadowColor: Colors.yellow,
+    textShadowOffset: { width: 2, height: 1 },
+    marginHorizontal: "8%",
+  },
+});
 
 export const css_Crucigrama = StyleSheet.create({
-    Container:{
-        flex:1,
-        backgroundColor: Colors.blue_dark,
-    },
-})
+  Container: {
+    flex: 1,
+    backgroundColor: Colors.blue_dark,
+  },
+});
 
 export const css_JuegoImagenes = StyleSheet.create({
-    Container:{
-        flex:1,
-        backgroundColor: Colors.blue_dark,
-    },
-    divImgOracion:{
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center'
-    },  
-    imgOracion:{
-        width: 87,
-        height: 90,
-    },
-    divJuego:{
-        margin: 8,
-        borderStyle: 'solid',
-        borderWidth: 3,
-        borderColor: Colors.yellow,
-        padding: 6,
-        borderRadius: 12,
-    },
-    divOracionesEjemplo:{
-        flexDirection:'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    Ejemplos:{
-        width: '30%', 
-        height: 100,
-        margin: '2%',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: Colors.redLight,
-        borderRadius: 10,
-        padding: '1%',
+  Container: {
+    flex: 1,
+    backgroundColor: Colors.blue_dark,
+  },
+  divImgOracion: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  imgOracion: {
+    width: 87,
+    height: 90,
+  },
+  divJuego: {
+    margin: 8,
+    marginTop: 14,
+    borderStyle: "solid",
+    borderWidth: 3,
+    borderColor: Colors.yellow,
+    padding: 6,
+    borderRadius: 12,
+  },
+  divOracionesEjemplo: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  Ejemplos: {
+    width: "30%",
+    height: 100,
+    margin: "2%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.redLight,
+    borderRadius: 10,
+    padding: "1%",
+  },
+  txtEjemplos: {
+    color: Colors.white,
+    textAlign: "center",
+    fontSize: 16,
+  },
+  InputOracion: {
+    height: 80,
+    borderColor: Colors.yellow,
+    borderWidth: 3,
+    borderRadius: 10,
+    margin: 8,
+    marginTop: 12,
+    padding: 10,
+    color: Colors.white,
+    fontSize: 16,
+  },
+  opcionesRuleta: {
+    width: 70,
+    height: 70,
+  },
+  divRuleta: {
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
+  divRuletaTop: {
+    // margin: '4%',
+    width: 170,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    display: "flex",
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "space-around",
+  },
+  divRuletaCenter: {
+    margin: "1%",
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  divRuletaCenter2: {
+    margin: "1%",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    flexWrap: "wrap",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  divRuletaDown: {
+    // margin: '2%',
+    width: 170,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    display: "flex",
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "space-around",
+  },
+  itemGiro: {
+    backgroundColor: Colors.turquesa,
+    borderRadius: 50,
+    opacity: 1,
+  },
+});
 
-    },
-    txtEjemplos:{
-        color: Colors.white,
-        textAlign: 'center',
-        fontSize: 16,
-    },
-    opcionesRuleta: {
-        width: 70,
-        height: 70,
-    },
-    divRuleta:{
-        paddingHorizontal: 30,
-        paddingVertical: 10
-    },
-    divRuletaTop:{
-        // margin: '4%',
-        width: 170,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        display: 'flex',
-        alignItems: 'center',
-        alignSelf: 'center',
-        justifyContent: 'space-around'
-    },
-    divRuletaCenter:{
-        margin: '1%',
-        paddingHorizontal: 10,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    divRuletaCenter2:{
-        margin: '1%',
-        flexDirection: 'row',
-        paddingHorizontal: 10,
-        flexWrap: 'wrap',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between'
-    },
-    divRuletaDown:{
-        // margin: '2%',
-        width: 170,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        display: 'flex',
-        alignItems: 'center',
-        alignSelf: 'center',
-        justifyContent: 'space-around'
-    },
-    itemGiro: {
-        backgroundColor: Colors.turquesa,
-        borderRadius: 50,
-        opacity: 1
-    }
-})
-
-export const css_Vocabulario =  StyleSheet.create({
-    Mensaje:{
-        flexDirection: 'row',
-        width: '70%',
-        alignSelf: 'center',
-        backgroundColor: Colors.yellow,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 50,
-        height: 80,
-        paddingLeft: 53,
-        marginVertical: '10%',
-    },
-    imgOso:{
-        width:120,
-        height: 120
-    },
-    txtMensaje:{
-        width: '62%',
-        color: Colors.white,
-        fontWeight: 'bold',
-        fontSize: 17
-    },
-    totalCajas:{
-        backgroundColor: Colors.red,
-        width: 170,
-        height: 30,
-        alignSelf: 'center',
-        alignItems: 'center',
-        justifyContent:'center',
-        flexDirection: 'row',
-        borderRadius: 12
-    },
-    txtCajas:{
-        color: Colors.white,
-        fontWeight: 'bold',
-        fontSize: 18
-    },
-    imgTotal:{
-        width: 50,
-        height: 50
-    },
-    divCajas:{
-        width: '42%',
-        padding: '8%',
-        margin: '4%',
-    },
-    caja: {
-        width:60,
-        height:60,
-        borderRadius: 8,
-        backgroundColor: Colors.turquesa,
-        justifyContent: 'center',
-        alignItems:'center'
-    },
-    imgCaja:{
-        width: '90%',
-        height: '90%',
-    },
-    containerCajas:{
-        flexDirection: "row",
-        width: '100%',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        alignItems: 'center',
-        alignContent: 'center',
-    }
-
-
-})
+export const css_Vocabulario = StyleSheet.create({
+  Mensaje: {
+    flexDirection: "row",
+    width: "70%",
+    alignSelf: "center",
+    backgroundColor: Colors.yellow,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+    height: 80,
+    paddingLeft: 53,
+    marginVertical: "10%",
+  },
+  imgOso: {
+    width: 120,
+    height: 120,
+  },
+  txtMensaje: {
+    width: "62%",
+    color: Colors.white,
+    fontWeight: "bold",
+    fontSize: 17,
+  },
+  totalCajas: {
+    backgroundColor: Colors.red,
+    width: 170,
+    height: 30,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    borderRadius: 12,
+  },
+  txtCajas: {
+    color: Colors.white,
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  imgTotal: {
+    width: 50,
+    height: 50,
+  },
+  divCajas: {
+    width: "42%",
+    padding: "8%",
+    margin: "4%",
+  },
+  caja: {
+    width: 60,
+    height: 60,
+    borderRadius: 8,
+    backgroundColor: Colors.turquesa,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imgCaja: {
+    width: "90%",
+    height: "90%",
+  },
+  containerCajas: {
+    flexDirection: "row",
+    width: "100%",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+});
 
 export const css_DeOposicion = StyleSheet.create({
-    SopaLetrasDiv:{
-        width: '100%',
-        height: '80%',
-    },
-    SopaLetrasOff: {
-        backgroundColor: Colors.turquesa,
-    },
-    SopaLetrasOn: {
-        backgroundColor: Colors.yellow
-    }
-})
+  SopaLetrasDiv: {
+    width: "100%",
+    height: "80%",
+  },
+  SopaLetrasOff: {
+    backgroundColor: Colors.turquesa,
+  },
+  SopaLetrasOn: {
+    backgroundColor: Colors.yellow,
+  },
+});
 
 export const css_DeLectura = StyleSheet.create({
   divTop: {
@@ -295,14 +302,14 @@ export const css_DeLectura = StyleSheet.create({
     borderRadius: 6,
     marginVertical: "2%",
   },
-  divNota:{
+  divNota: {
     // backgroundColor: Colors.turquesa,
     padding: 10,
     borderRadius: 6,
     marginVertical: "2%",
-    flexDirection: 'row',
-    justifyContent:'center',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   txtPregunta: {
     color: Colors.white,
@@ -333,64 +340,64 @@ export const css_DeLectura = StyleSheet.create({
 });
 
 export const css_ItemCrucigrama = StyleSheet.create({
-    cell: {
-        padding: 2,
-        backgroundColor: Colors.turquesa,
-        width: 45,
-        borderColor: Colors.blue_dark,
-        borderWidth: 1,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      cellError: {
-        padding: 2,
-        backgroundColor: Colors.redLight,
-        width: 45,
-        borderColor: Colors.blue_dark,
-        borderWidth: 1,
-        height: 40,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      cellPress: {
-        padding: 2,
-        backgroundColor: Colors.yellow,
-        width: 45,
-        height: 40,
-        borderColor: Colors.blue_dark,
-        borderWidth: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      cellText: {
-        color: Colors.white,
-        fontSize: 22,
-        textTransform: "uppercase",
-        fontWeight: "bold",
-        textAlign: "center",
-      },
-      InputNombre:{
-          fontSize: 24,
-          textAlign: 'center',
-          color: Colors.white,
-          width: 45,
-          fontWeight: 'bold',
-      },
-      cellEmpty:{
-          padding: 2,
-          width: 45,
-          height: 40,
-          alignItems: "center",
-          justifyContent: "center",
-      },
-      cellTextEmpty:{
+  cell: {
+    padding: 2,
+    backgroundColor: Colors.turquesa,
+    width: 45,
+    borderColor: Colors.blue_dark,
+    borderWidth: 1,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellError: {
+    padding: 2,
+    backgroundColor: Colors.redLight,
+    width: 45,
+    borderColor: Colors.blue_dark,
+    borderWidth: 1,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellPress: {
+    padding: 2,
+    backgroundColor: Colors.yellow,
+    width: 45,
+    height: 40,
+    borderColor: Colors.blue_dark,
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellText: {
+    color: Colors.white,
+    fontSize: 22,
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  InputNombre: {
+    fontSize: 24,
+    textAlign: "center",
+    color: Colors.white,
+    width: 45,
+    fontWeight: "bold",
+  },
+  cellEmpty: {
+    padding: 2,
+    width: 45,
+    height: 40,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cellTextEmpty: {
     opacity: 1,
     color: Colors.white,
     fontSize: 0,
     textTransform: "uppercase",
     fontWeight: "bold",
     textAlign: "center",
-      },
-      imgFruta: {width: 45, height:40, position: 'absolute'},
-})
+  },
+  imgFruta: { width: 45, height: 40, position: "absolute" },
+});
